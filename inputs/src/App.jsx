75c6cd42103a3b33c4c15c93304dc7c68f49d3a6
Yuliana del Carmen Altamirano Montes <isc20350250@gmail.com>
2024-03-14 11:57:51 -0600

    inputs

---
+++ inputs/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+function App(){
+  const [username, setUsername] = useState("")
+  const handleUsernameChange =(event) =>{
+    setUsername (event.target.value)
+    //console.log(username)
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDedault()
+    console.log(username)
+    setUsername ("")
+  }
+  return(  
+  <div>
+    <h1>Inputs</h1>
+    <hr />
+    <form onSubmit ={(e) => handleSubmit(e)}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input 
+        id="username"
+         type="text"
+         value={username}
+         onChange={(event) => handleUsernameChange (event)}
+         />
+
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <button type ="submit">Submit</button>
+    </form>
+  </div>
+  )
+}
+
+export default App
