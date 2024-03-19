01876dee6f456f13b45458f2795270acd19e40de
Yuliana del Carmen Altamirano Montes <isc20350250@gmail.com>
2024-03-19 11:44:48 -0600

    optimizacion de inputs

--- inputs/src/App.jsx
+++ inputs/src/App.jsx
@@ -1,40 +1,58 @@
 import { useState } from "react"
 
-function App(){
-  const [username, setUsername] = useState("")
-  const handleUsernameChange =(event) =>{
-    setUsername (event.target.value)
-    //console.log(username)
 
+function App() {
+  const [inputsForm, setInputsForm] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleInputChange = (event) => {
+    setInputsForm({
+      ...inputsForm,
+      [event.target.name]: event.target.value,
+    })
   }
-  const handleSubmit = (e) => {
-    e.preventDedault()
-    console.log(username)
-    setUsername ("")
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log("Nombre de usuario:", inputsForm.username)
+    console.log("Contraseña:", inputsForm.password)
+    setInputsForm({
+      username: "",
+      password: "",
+    })
   }
-  return(  
+
+return(
   <div>
     <h1>Inputs</h1>
     <hr />
-    <form onSubmit ={(e) => handleSubmit(e)}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <div>
         <label htmlFor="username">Username</label>
-        <input 
-        id="username"
-         type="text"
-         value={username}
-         onChange={(event) => handleUsernameChange (event)}
-         />
-
+        <input  
+        id="username" 
+        type="text" 
+        name="username"
+        value={inputsForm.username}
+        onChange={(event)=> handleInputChange(event)}
+        />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <input 
+        id="password" 
+        type="password" 
+        name="password"
+        value={inputsForm.password}
+        onChange={(event) => handleInputChange(event)}
+        />
       </div>
-      <button type ="submit">Submit</button>
+      <button type="submit">Submit</button>
     </form>
   </div>
-  )
+)
 }
 
 export default App
