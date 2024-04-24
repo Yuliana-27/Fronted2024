cf954b1ce2632df186241904f1f9a68ac8209aea
Yuliana del Carmen Altamirano Montes <isc20350250@gmail.com>
2024-04-24 11:59:11 -0600

    manejo del LocalStorage

--- ToDoList/src/TaksModal.jsx
+++ ToDoList/src/TaksModal.jsx
@@ -6,8 +6,21 @@ const taskInfo ={
     limit: "",
 }
 
-const TaskModal = () => {
+const TaskModal = ({taskList, setTaskList}) => {
     const [values, handleInputChange, reset] = useForm(taskInfo)
+
+    const handleSaveClick = () => {
+        setTaskList([
+            ...taskList,
+            {
+            id: taskList.length + 1,
+            ...values,
+            isDone: false
+            }
+        ])
+        localStorage.setItem("taskList", JSON.stringify(taskList))
+        reset()
+    }
     return(
         <div className="modal fade" id={"taskModal"}>
             <div className="modal-dialog mdoal-dialog-centered">
@@ -81,7 +94,7 @@ const TaskModal = () => {
                 </div>
                 <div className="modal-footer">
                     <button className="btn btn-sm btn-outline-primary"
-                    onClick={() => console.log(values)}
+                    onClick={handleSaveClick}
                     >
                     <i className="bi bi-pencil-square"></i> 
                     Save
